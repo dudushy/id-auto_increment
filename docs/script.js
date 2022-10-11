8e6314c9e34a4005fe41e1b2931af524edf98682
dudushy <eduardotalarico1337@gmail.com>
2022-10-11 00:15:52 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,10 @@
-console.log("[template-web] Hello World!");
+console.log("[id-auto_increment] init");
+
+function generateId() {
+  var idSpan = document.getElementById("idSpan");
+  var id = idSpan.innerText;
+
+  newId = parseInt(id) + 1;
+
+  idSpan.innerText = newId;
+}
